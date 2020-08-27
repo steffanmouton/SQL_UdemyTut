c05fde2aa15080cf1cae27ffa75f9a2b14f2a492
Steffan M <36455372+steffanmouton@users.noreply.github.com>
2020-08-27 17:21:17 -0500

    Update SQLAssessment1.sql
    adjusted indents

--- SQLAssessment1.sql
+++ SQLAssessment1.sql
@@ -25,14 +25,14 @@ Q2:
 How many films begin with the letter J?
 */
 SELECT COUNT(*) FROM film
-  WHERE title LIKE 'J%';
+  	WHERE title LIKE 'J%';
   
 /*
 Q3:
 What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500?
 */
 SELECT customer_id, first_name, last_name FROM customer
-  WHERE first_name LIKE 'E%'
-  AND address_id < 500
-  ORDER BY customer_id DESC
-  LIMIT 1;
+  	WHERE first_name LIKE 'E%'
+  	AND address_id < 500
+	ORDER BY customer_id DESC
+  	LIMIT 1;
